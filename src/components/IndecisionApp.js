5f68885b9ce3b5049808e2bffab6be231de7b75b
Ryo <kanadajinlee@yahoo.ca>
2017-10-20 20:04:00 -0400

    Finished styling all of the component parts (except the modal, since it's 3rd-party)

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -68,19 +68,23 @@ export default class IndecisionApp extends Component {
 
     return(
       <div>
-        <Header subtitle={subtitle}/>
-        <Action 
-          handlePick={this.handlePick}
-          hasOptions={this.state.options.length > 0}
-        />
-        <Options 
-          options={this.state.options}
-          handleDeleteOptions={this.handleDeleteOptions}
-          handleDeleteOption={this.handleDeleteOption}
-        />
-        <AddOption 
-          handleAddOption={this.handleAddOption}
-        />
+        <Header subtitle={subtitle} />
+        <div className="container">
+          <Action 
+            handlePick={this.handlePick}
+            hasOptions={this.state.options.length > 0}
+          />
+          <div className="widget">
+            <Options 
+              options={this.state.options}
+              handleDeleteOptions={this.handleDeleteOptions}
+              handleDeleteOption={this.handleDeleteOption}
+            />
+            <AddOption 
+              handleAddOption={this.handleAddOption}
+            />
+          </div>
+        </div>
         <OptionModal 
           selectedOption={this.state.selectedOption}
           handleClearSelectedOption={this.handleClearSelectedOption}
