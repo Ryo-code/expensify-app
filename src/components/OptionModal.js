7a88457da269d4a57a821fc9786483ad1ac7b06e
Ryo <kanadajinlee@yahoo.ca>
2017-10-20 20:23:29 -0400

    Styled the 3rd-party React-Modal component

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -6,10 +6,12 @@ const OptionModal = (props) => (
     isOpen={!!props.selectedOption} //adding "!!" forces this to become a boolean
     onRequestClose={props.handleClearSelectedOption} //triggered when user hits ESC or clicks off modal
     contentLabel="Selected Option"
+    closeTimeoutMS={200}
+    className="modal" //ReactModal lets us use our own styles 
   >
-    <h3>Selection Option</h3>
-    {props.selectedOption && <p>{props.selectedOption}</p>}
-    <button onClick={props.handleClearSelectedOption}>Okay</button>
+    <h3 className="modal__title">Selection Option</h3>
+    {props.selectedOption && <p className="modal__body">{props.selectedOption}</p>}
+    <button className="button" onClick={props.handleClearSelectedOption}>Okay</button>
   </Modal>
 );
 
