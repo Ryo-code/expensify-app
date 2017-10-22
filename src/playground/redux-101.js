236933658964a1a57fdc7961f9c13f5f69501597
Ryo <kanadajinlee@yahoo.ca>
2017-10-22 16:58:17 -0400

    Dispatched dynamic actions (and used subscribe to watch for changes to the store)

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -3,12 +3,18 @@ import { createStore } from "redux";
 const store = createStore((state = { count: 0 }, action) => {
   switch (action.type){
     case 'INCREMENT':
+      const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
       return {
-        count: state.count + 1
+        count: state.count + incrementBy
       };
     case 'DECREMENT':
+      const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
       return {
-        count: state.count -1
+        count: state.count - decrementBy
+      }
+    case 'SET':
+      return {
+        count: action.count
       }
     case 'RESET':
       return {
@@ -19,22 +25,30 @@ const store = createStore((state = { count: 0 }, action) => {
   }
 });
 
-console.log(store.getState());
-
-store.dispatch({
-  type: 'INCREMENT'
+const unsubscribe = store.subscribe(() => { //subscribe waits for changes to the store
+  console.log(store.getState());
 });
 
 store.dispatch({
-  type: 'INCREMENT'
+  type: 'INCREMENT',
+  incrementBy: 5
 });
 
 store.dispatch({
-  type: 'INCREMENT'
+  type: 'DECREMENT',
+  decrementBy: 3
 });
 
+store.dispatch({
+  type: 'DECREMENT'
+});
 
+store.dispatch({
+  type: 'RESET'
+});
 
-//reset count
+store.dispatch({
+  type: 'SET',
+  count: 101
+});
 
-console.log(store.getState());
