d87385990e1a6295a4fde3c7f21cc4eaec404bd0
Ryo <kanadajinlee@yahoo.ca>
2017-11-03 10:11:40 -0400

    Created addExpense functionality (and router redirect with push)

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import { connect } from "react-redux";
 import ExpenseForm from "./ExpenseForm";
+import { addExpense } from "../actions/expenses";
 
-const AddExpensePage = () => {
+const AddExpensePage = (props) => {
   return (
     <div>
       <h1>Add Expense</h1>
-      <ExpenseForm />
+      <ExpenseForm 
+        onSubmit={(expense) => {
+          console.log(expense);
+          props.dispatch(addExpense(expense));
+          props.history.push("/"); //switch over user browser-routing (so no page-refresh)
+        }}
+      />
     </div>
   )
 };
 
-export default AddExpensePage
+//We can leave the first parentheses empty because we don't need anything from the state
+export default connect()(AddExpensePage);
